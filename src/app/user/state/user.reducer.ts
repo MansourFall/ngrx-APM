91501edb1e0c692e7ea4568c0663379ef29acfbc
Mansour FALL <fallmansour19@yahoo.fr>
2022-09-18 16:17:21 +0000

    feat: added selectors for user feature

--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,15 +1,26 @@
-import { createAction, createReducer, on } from "@ngrx/store";
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from '@ngrx/store';
 
 export interface UserState {
-    maskUserName: boolean;
+  maskUserName: boolean;
 }
 
-export const userReducer = createReducer(
-    {maskUserName: false},
-   on(createAction('[User] Mask User Name'), state => {
+const initialState: UserState = {
+  maskUserName: true,
+};
+
+const getUserFeatureState = createFeatureSelector<UserState>('user');
+
+export const getMaskUserName = createSelector(
+    getUserFeatureState,
+    state => state.maskUserName
+);
+
+export const userReducer = createReducer<UserState>(
+  initialState,
+  on(createAction('[User] Mask User Name'), (state) => {
     return {
-        ...state,
-        maskUserName: !state.maskUserName
-    }
-   })
+      ...state,
+      maskUserName: !state.maskUserName,
+    };
+  })
 );
